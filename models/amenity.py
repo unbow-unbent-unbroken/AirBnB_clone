bdba3ebed99f2ca3f26da89f8e17848b07edc48f
godfreydekew <dekewgodfrey@gmail.com>
2024-02-11 09:54:52 -0800

    Amenity class

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Amenity class"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class inherits from BaseModel.
+
+    Public Class Attributes:
+        name (str): Empty string.
+    """
+    name = ""
