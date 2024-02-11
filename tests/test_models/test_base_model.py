e4edfd3f80f583ba047fe092c55147858c3c7c1c
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-02-11 09:05:41 -0800

    updating test base model file

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,211 @@
+#!/usr/bin/python3
+""" Defines unittests for models/base_model.py. """
+
+import unittest
+from time import sleep
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestBaseModelInit(unittest.TestCase):
+    def test_obj_type(self):
+        """Verify that the object is an instance of BaseModel."""
+        obj = BaseModel()
+        self.assertEqual(type(obj), BaseModel)
+
+    def test_multiple_instances(self):
+        """Test handling multiple instances."""
+        bm1 = BaseModel()
+        bm2 = BaseModel()
+
+        self.assertNotEqual(bm1.id, bm2.id)
+        bm1.name = "Object1"
+        bm2.name = "Object2"
+
+        self.assertEqual(bm1.name, "Object1")
+        self.assertEqual(bm2.name, "Object2")
+
+    def test_no_class_attribute(self):
+        """Test if __class__ attribute is not set in the __init__ method."""
+        kwargs = {
+            'id': 'test_id',
+            'created_at': '2024-02-11T12:34:56',
+            'updated_at': '2024-02-11T12:45:00',
+            'custom_field': 'custom_value',
+            '__class__': 'ShouldNotBeSet'
+        }
+
+        instance = BaseModel(**kwargs)
+        self.assertNotIn('__class__', instance.__dict__)
+
+    def test_id_type(self):
+        """Verify that the 'id' attribute is of type str."""
+        obj = BaseModel()
+        self.assertEqual(type(obj.id), str)
+
+    def test_created_type(self):
+        """Verify that the 'created_at' attribute is of type datetime."""
+        obj = BaseModel()
+        self.assertEqual(type(obj.created_at), datetime)
+
+    def test_updated_type(self):
+        """Verify that the 'updated_at' attribute is of type datetime."""
+        obj = BaseModel()
+        self.assertEqual(type(obj.updated_at), datetime)
+
+    def test_different_id(self):
+        """Verify that two instances have different 'id' values."""
+        x = BaseModel()
+        y = BaseModel()
+        self.assertNotEqual(x.id, y.id)
+
+    def test_public_attribute(self):
+        """Test case to check if setting a public attribute works"""
+        instance = BaseModel()
+        instance.name = "godie"
+        self.assertEqual(instance.name, "godie")
+
+    def test_update_at(self):
+        """Verify that 'updated_at' changes after updating
+        an attribute and saving."""
+        x = BaseModel()
+        sleep(1)
+        x.name = "godie"
+        x.save()
+        self.assertNotEqual(x.created_at, x.updated_at)
+
+    def test_update_attributes(self):
+        """Verify that attributes can be updated and saved."""
+        kwargs = {
+            'name': 'Godfrey',
+            'age': 23,
+        }
+
+        obj = BaseModel(**kwargs)
+        self.assertEqual(obj.name, "Godfrey")
+        obj.name = "Done"
+        self.assertEqual(obj.name, "Done")
+
+    def test_created_different_time(self):
+        """Verify that 'created_at' times are different for
+            two instances created with a time gap.
+        """
+        x = BaseModel()
+        sleep(1)
+        y = BaseModel()
+        self.assertNotEqual(x.created_at, y.created_at)
+
+    def test_updated_different_time(self):
+        """Verify that 'updated_at' times are
+            different after saving
+         two instances with different changes.
+         """
+        x = BaseModel()
+        y = BaseModel()
+        x.name = "test"
+        x.save()
+        y.name = "pass"
+        y.save()
+        self.assertNotEqual(x.updated_at, y.updated_at)
+
+    def test_create_with_kwargs(self):
+        """Test case when kwargs is provided"""
+        kwargs = {
+            'id': 'test_id',
+            'created_at': '2024-02-11T12:34:56',
+            'updated_at': '2024-02-11T12:45:00',
+            'custom_field': 'custom_value'
+        }
+
+        instance = BaseModel(**kwargs)
+
+        self.assertEqual(instance.id, 'test_id')
+        self.assertEqual(instance.custom_field, 'custom_value')
+        self.assertEqual(instance.created_at,
+                         datetime.fromisoformat('2024-02-11T12:34:56'))
+        self.assertEqual(instance.updated_at,
+                         datetime.fromisoformat('2024-02-11T12:45:00'))
+
+    def test_create_with_kwargs_type(self):
+        """Test creating a new instance"""
+        kwargs = {
+            'name': 'Godfrey',
+            'age': 23,
+        }
+        obj = BaseModel(**kwargs)
+        self.assertEqual(type(obj.age), int)
+        self.assertEqual(type(obj.name), str)
+
+    def test_kwargs_values(self):
+        """Test kwargs values"""
+        kwargs = {
+            'name': 'Godfrey',
+            'age': 23,
+        }
+        obj = BaseModel(**kwargs)
+        self.assertEqual(obj.name, "Godfrey")
+        self.assertEqual(obj.age, 23)
+
+    def test_print_class(self):
+        """Test case to check if the class can be printed"""
+        instance = BaseModel()
+        printed_output = str(instance)
+        self.assertIn("BaseModel", printed_output)
+        self.assertIn("id", printed_output)
+        self.assertIn("created_at", printed_output)
+        self.assertIn("updated_at", printed_output)
+
+    def test_docstring(self):
+        """Test case to check if the class contains a docstring"""
+        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertNotEqual(BaseModel.__doc__, "")
+
+    def test_to_dict_method(self):
+        """Test case for the to_dict method"""
+        instance = BaseModel()
+        instance.name = "godie"
+        instance.created_at = datetime(2024, 2, 11, 12, 34, 56)
+        instance.updated_at = datetime(2024, 2, 11, 12, 45, 0)
+
+        expected_dict = {
+            'id': instance.id,
+            'created_at': '2024-02-11T12:34:56',
+            'updated_at': '2024-02-11T12:45:00',
+            'name': 'godie',
+            '__class__': 'BaseModel'
+        }
+
+        self.assertEqual(instance.to_dict(), expected_dict)
+
+    def test_to_dict_default_values(self):
+        """Test case for to_dict method with default values"""
+        instance = BaseModel()
+
+        expected_dict = {
+            'id': instance.id,
+            'created_at': instance.created_at.isoformat(),
+            'updated_at': instance.updated_at.isoformat(),
+            '__class__': 'BaseModel'
+        }
+
+        self.assertEqual(instance.to_dict(), expected_dict)
+
+    def test_to_dict_with_custom_attributes(self):
+        """Test case for to_dict method with custom attributes"""
+        instance = BaseModel()
+
+        instance.custom_field = 'custom_value'
+
+        expected_dict = {
+            'id': instance.id,
+            'created_at': instance.created_at.isoformat(),
+            'updated_at': instance.updated_at.isoformat(),
+            'custom_field': 'custom_value',
+            '__class__': 'BaseModel'
+        }
+
+        self.assertEqual(instance.to_dict(), expected_dict)
+
+
+if __name__ == "__main__":
+    unittest.main()
