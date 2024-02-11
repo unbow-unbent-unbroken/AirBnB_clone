e94f3829939c07aeee4c9fb304e66936b7a0a849
godfreydekew <dekewgodfrey@gmail.com>
2024-02-11 10:03:20 -0800

    State class

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines the child state"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    State class inherits from BaseModel.
+
+    Public Class Attributes:
+        name (str): Empty string.
+    """
+    name = ""
